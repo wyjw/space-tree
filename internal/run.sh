fd0190ee21ff4825146d977f4d7c3c256082f105
wyjw <yujian.wu1@gmail.com>
2020-09-14 06:23:15 +0800

    add new percona directory, which is the block device stuff

--- internal/run.sh
+++ internal/run.sh
@@ -1,1 +1,7 @@
 LD_PRELOAD="../PerconaFT/build/ft/libft.so ../PerconaFT/build/portability/libtokuportability.so" ./fttest 40000 3200 1
+LD_PRELOAD="../PerconaFT/build/ft/libft.so ../PerconaFT/build/portability/libtokuportability.so" ./fttest_block_alloc
+LD_PRELOAD="../PerconaFT/build/ft/libft.so ../PerconaFT/build/portability/libtokuportability.so" ./fttest_bench
+
+LD_PRELOAD="../PerconaInBlockFT/build/ft/libft.so ../PerconaInBlockFT/build/portability/libtokuportability.so" ./fttest 40000 3200 1
+LD_PRELOAD="../PerconaInBlockFT/build/ft/libft.so ../PerconaInBlockFT/build/portability/libtokuportability.so" ./fttest_block_alloc
+LD_PRELOAD="../PerconaInBlockFT/build/ft/libft.so ../PerconaInBlockFT/build/portability/libtokuportability.so" ./fttest_bench
