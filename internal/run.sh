84e1d9f4f389e705f55930a56d4784b666674c65
wyjw <yujian.wu1@gmail.com>
2020-10-10 02:16:21 +0800

    change run file

--- internal/run.sh
+++ internal/run.sh
@@ -6,6 +6,6 @@ LD_PRELOAD="../PerconaInBlockFT/build/ft/libft.so ../PerconaInBlockFT/build/port
 LD_PRELOAD="../PerconaInBlockFT/build/ft/libft.so ../PerconaInBlockFT/build/portability/libtokuportability.so" ./fttest_block_alloc
 LD_PRELOAD="../PerconaInBlockFT/build/ft/libft.so ../PerconaInBlockFT/build/portability/libtokuportability.so" ./fttest_bench
 
-LD_PRELOAD="../PerconaWithUringFT/build/ft/libft.so ../PerconaWithUringFT/build/portability/libtokuportability.so" ./fttest 40000 3200 1
-LD_PRELOAD="../PerconaWithUringFT/build/ft/libft.so ../PerconaWithUringFT/build/portability/libtokuportability.so" ./fttest_block_alloc
-LD_PRELOAD="../PerconaWithUringFT/build/ft/libft.so ../PerconaWithUringFT/build/portability/libtokuportability.so" ./fttest_bench
+#LD_PRELOAD="../PerconaWithUringFT/build/ft/libft.so ../PerconaWithUringFT/build/portability/libtokuportability.so" ./fttest 40000 3200 1
+#LD_PRELOAD="../PerconaWithUringFT/build/ft/libft.so ../PerconaWithUringFT/build/portability/libtokuportability.so" ./fttest_block_alloc
+#LD_PRELOAD="../PerconaWithUringFT/build/ft/libft.so ../PerconaWithUringFT/build/portability/libtokuportability.so" ./fttest_bench
