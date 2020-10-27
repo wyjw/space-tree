19cf9df92d7956b7b9c13b53f90255ca2ac9aba7
wyjw <yujian.wu1@gmail.com>
2020-10-27 12:02:25 +0800

    add ioctl stuff

---
+++ tests/treenvme_ioctl.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+// fake struct (not real, don't use)
+struct blck_tbl {
+	int a;
+};
+
+int main(void) {
+	int fd;
+	
+	struct blck_tbl tbl;	
+	tbl.a = 4;
+
+	//for (int i = 0; i)
+	fd = open("/dev/treenvme0", O_RDWR);
+	ioctl(fd, TREENVME_IOCTL_REGISTER_BLOCKTABLE, &tbl);	
+}
