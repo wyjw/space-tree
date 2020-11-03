c955942977c3a194a6fb0cdb0419d5fddfbca9e3
wyjw <yujian.wu1@gmail.com>
2020-11-03 17:59:22 +0800

    here

--- withperconatests/run.sh
+++ withperconatests/run.sh
@@ -1,1 +1,2 @@
-LD_PRELOAD="../PerconaInBlockFT/build/ft/libft.so ../PerconaInBlockFT/build/portability/libtokuportability.so" ./ftstar 30 30 5 5
+#LD_PRELOAD="../PerconaInBlockFT/build/ft/libft.so ../PerconaInBlockFT/build/portability/libtokuportability.so" ./ftstar 30 30 5 5
+LD_PRELOAD="../PerconaInBlockFT/build/ft/libft.so ../PerconaInBlockFT/build/portability/libtokuportability.so" ./ftstar2 30 30 5 5
